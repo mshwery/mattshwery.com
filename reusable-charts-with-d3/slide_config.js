5a9f3193e9393addb707e2301667d4a26c47a62b
Matt Shwery <mshwery@gmail.com>
2014-05-31 20:21:13 -0400

    added all the things for the self.conference talk

---
+++ reusable-charts-with-d3/slide_config.js
@@ -0,0 +1,33 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Reusable Charts',
+    subtitle: 'with d3.js',
+    eventInfo: {
+     title: 'self.conference',
+     date: '2014'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/federated_sample_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['fed']
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Matt Shwery',
+    company: 'Federated Sample',
+    twitter: '@mshwery',
+    www: 'http://mattshwery.com',
+    github: 'http://github.com/mshwery'
+  }]
+};
+
